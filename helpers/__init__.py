73f0f8e24644dc52f67b41a9d60d7412951e6f31
Érico Andrei <ericof@gmail.com>
2023-05-08 10:06:23 -0300

    py5.get is now py5.get_pixels

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -24,7 +24,7 @@ def save_image(img_name, extension="jpg"):
     folder = Path(__file__).parent.parent.resolve()
     img_folder = folder / "Images"
     img_path = img_folder / f"{img_name}.{extension}"
-    img = py5.get(0, 0, WIDTH, HEIGHT)
+    img = py5.get_pixels(0, 0, WIDTH, HEIGHT)
     img.save(img_path)
 
 
