a6a959595ad3ef45ece20cc6461c9ad6a9453b6e
Érico Andrei <ericof@gmail.com>
2024-04-06 19:09:12 +0300

    Sketch for 2024-04-06

--- README.md
+++ README.md
@@ -57,7 +57,9 @@ You can follow this project also on:
 | [2024-04-03](./sketches/d2024_04_03) | Mosaico | [./sketches/d2024_04_03/__main__.py](./sketches/d2024_04_03/__main__.py) | ![2024-04-03](./sketches/d2024_04_03/2024-04-03.png) |
 | [2024-04-04](./sketches/d2024_04_04) | Mosaico II | [./sketches/d2024_04_04/__main__.py](./sketches/d2024_04_04/__main__.py) | ![2024-04-04](./sketches/d2024_04_04/2024-04-04.png) |
 | [2024-04-05](./sketches/d2024_04_05) | Mosaico III | [./sketches/d2024_04_05/__main__.py](./sketches/d2024_04_05/__main__.py) | ![2024-04-05](./sketches/d2024_04_05/2024-04-05.png) |
+| [2024-04-06](./sketches/d2024_04_06) | Mosaico IV | [./sketches/d2024_04_06/__main__.py](./sketches/d2024_04_06/__main__.py) | ![2024-04-06](./sketches/d2024_04_06/2024-04-06.png) |
 <!-- Next Item -->
+
 
 
 
---
+++ sketches/d2024_04_06/__main__.py
@@ -0,0 +1,82 @@
+"""2024-04-06
+Mosaico IV
+Exercício de criação de um mosaico em uma grade, utilizando elipses.
+png
+Sketch,py5,CreativeCoding,grid
+"""
+
+import py5
+
+from utils import helpers
+
+sketch = helpers.info_for_sketch(__file__, __doc__)
+
+
+def cria_grade(
+    margem_x: int, margem_y: int, celula_x: int, celula_y: int, alternada: bool = True
+):
+    """Cria uma grade."""
+    pontos = []
+    celula_x = int(celula_x)
+    celula_y = int(celula_y)
+    yi = margem_y
+    yf = py5.height - margem_y
+    for idy, y in enumerate(range(yi, yf, celula_y)):
+        if (y + celula_y) > yf:
+            break
+        buffer = int(celula_x / 2) if (alternada and idy % 2) else 0
+        xi = margem_x - buffer
+        xf = py5.width - margem_x
+        for x in range(xi, xf, celula_x):
+            pontos.append((x, y))
+    return pontos
+
+
+def centro_celula(x0: int, y0: int, largura: int, altura: int) -> tuple[float, float]:
+    """Calcula o centro de uma célula da grade."""
+    x = x0 + (largura / 2)
+    y = y0 + (altura / 2)
+    return x, y
+
+
+def setup():
+    py5.size(helpers.LARGURA, helpers.ALTURA)
+    py5.color_mode(py5.HSB, 360, 100, 100)
+    py5.background(0, 80, 20)
+    py5.ellipse_mode(py5.CENTER)
+    margem_x = -20
+    margem_y = -20
+    largura = 37
+    altura = 37
+    grade = cria_grade(margem_x, margem_y, largura, altura, True)
+    for x, y in grade:
+        h = py5.random_int(280, 300)
+        s = py5.random_int(0, 2)
+        b = py5.random_int(40, 70)
+        py5.fill(h, s, b)
+        x, y = centro_celula(x, y, largura, altura)
+        escala_x = py5.random(0.6, 1.2)
+        escala_y = py5.random(0.4, 1.2)
+        angulo = py5.random_int(-60, 60)
+        forma = py5.create_shape(
+            py5.ELLIPSE, 0, 0, largura * escala_x, altura * escala_y
+        )
+        forma.rotate(py5.radians(angulo))
+        py5.shape(forma, x, y)
+    helpers.write_legend(sketch=sketch, cor="#FFF")
+
+
+def key_pressed():
+    key = py5.key
+    if key == " ":
+        save_and_close()
+
+
+def save_and_close():
+    py5.no_loop()
+    helpers.save_sketch_image(sketch)
+    py5.exit_sketch()
+
+
+if __name__ == "__main__":
+    py5.run_sketch()
