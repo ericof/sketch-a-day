d52d8bc2b48066dbd1eac8ae258d539aa768e08f
Érico Andrei <ericof@gmail.com>
2024-04-08 10:38:37 -0300

    Support passing today as the day to the create command

--- utils/cli/create.py
+++ utils/cli/create.py
@@ -1,9 +1,12 @@
 import shutil
+from datetime import date
 from pathlib import Path
 
 
 def create_new_sketch(day: str):
     """Create a new sketch from template."""
+    if day.lower() == "today":
+        day = f"{date.today()}"
     formatted_day = f"d{day.replace('-', '_')}"
     src = Path(".") / "sketches" / "_template"
     dst = Path(".") / "sketches" / formatted_day
