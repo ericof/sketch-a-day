c19cf038df11d6a4154b4c2bfedfc3124a469d0a
Érico Andrei <ericof@gmail.com>
2024-04-09 18:20:30 +0300

    Sketch for 2024-04-09

---
+++ sketches/d2024_04_09/__main__.py
@@ -0,0 +1,85 @@
+"""2024-04-09
+Mosaico VII
+Exercício de criação de um mosaico em grade, utilizando quadriláteros.
+png
+Sketch,py5,CreativeCoding,grid
+"""
+
+import py5
+
+from utils import helpers
+
+sketch = helpers.info_for_sketch(__file__, __doc__)
+
+
+def cria_grade(
+    margem_x: int, margem_y: int, celula_x: int, celula_y: int, alternada: bool = True
+):
+    """Cria uma grade."""
+    pontos = []
+    celula_x = int(celula_x)
+    celula_y = int(celula_y)
+    yi = margem_y
+    yf = py5.height - margem_y
+    for idy, y in enumerate(range(yi, yf, celula_y)):
+        if (y + celula_y) > yf:
+            break
+        buffer = int(celula_x / 2) if (alternada and idy % 2) else 0
+        xi = margem_x - buffer
+        xf = py5.width - margem_x
+        for x in range(xi, xf, celula_x):
+            pontos.append((x, y))
+    return pontos
+
+
+def centro_celula(x0: int, y0: int, largura: int, altura: int) -> tuple[float, float]:
+    """Calcula o centro de uma célula da grade."""
+    x = x0 + (largura / 2)
+    y = y0 + (altura / 2)
+    return x, y
+
+
+def setup():
+    py5.size(helpers.LARGURA, helpers.ALTURA)
+    py5.color_mode(py5.HSB, 360, 100, 100)
+    py5.background(180, 80, 80)
+    py5.ellipse_mode(py5.CENTER)
+    margem_x = -20
+    margem_y = -20
+    largura = 41
+    altura = 41
+    grade = cria_grade(margem_x, margem_y, largura, altura, False)
+    py5.no_stroke()
+    for x, y in grade:
+        h = py5.random_int(60, 90)
+        s = py5.random_int(60, 70)
+        b = py5.random_int(40, 70)
+        py5.fill(h, s, b)
+        s -= 30
+        x, y = centro_celula(x, y, largura, altura)
+        escala_x = py5.random(0.6, 0.7)
+        escala_y = py5.random(0.3, 0.5)
+        angulo = py5.random_int(30, 75)
+        for modificador in (1, -1):
+            forma = py5.create_shape(
+                py5.RECT, 0, 0, largura * escala_x, altura * escala_y
+            )
+            forma.rotate(py5.radians(angulo * modificador))
+            py5.shape(forma, x, y)
+    helpers.write_legend(sketch=sketch, cor="#FFF", frame="#000")
+
+
+def key_pressed():
+    key = py5.key
+    if key == " ":
+        save_and_close()
+
+
+def save_and_close():
+    py5.no_loop()
+    helpers.save_sketch_image(sketch)
+    py5.exit_sketch()
+
+
+if __name__ == "__main__":
+    py5.run_sketch()
