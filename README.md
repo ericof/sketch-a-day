ba5cc708eeb7af537805bf3275ce84bf7d6ce0b6
Érico Andrei <ericof@gmail.com>
2024-06-05 15:01:06 +0300

    Sketch for 2024-06-05

--- README.md
+++ README.md
@@ -117,7 +117,9 @@ You can follow this project also on:
 | [2024-06-02](./sketches/d2024_06_02) | Grades sobrepostas 4 | [./sketches/d2024_06_02/__main__.py](./sketches/d2024_06_02/__main__.py) | ![2024-06-02](./sketches/d2024_06_02/2024-06-02.png) |
 | [2024-06-03](./sketches/d2024_06_03) | Grades sobrepostas 5 | [./sketches/d2024_06_03/__main__.py](./sketches/d2024_06_03/__main__.py) | ![2024-06-03](./sketches/d2024_06_03/2024-06-03.png) |
 | [2024-06-04](./sketches/d2024_06_04) | Grades sobrepostas 6 | [./sketches/d2024_06_04/__main__.py](./sketches/d2024_06_04/__main__.py) | ![2024-06-04](./sketches/d2024_06_04/2024-06-04.png) |
+| [2024-06-05](./sketches/d2024_06_05) | Grades sobrepostas 7 | [./sketches/d2024_06_05/__main__.py](./sketches/d2024_06_05/__main__.py) | ![2024-06-05](./sketches/d2024_06_05/2024-06-05.png) |
 <!-- Next Item -->
+
 
 
 
---
+++ sketches/d2024_06_05/__main__.py
@@ -0,0 +1,96 @@
+"""2024-06-05
+Grades sobrepostas 7
+Exercício de criação de grades sobrepostas
+png
+Sketch,py5,CreativeCoding
+"""
+
+from dataclasses import dataclass
+
+import py5
+
+from utils import helpers
+
+sketch = helpers.info_for_sketch(__file__, __doc__)
+
+
+@dataclass
+class Celula:
+    x: int
+    y: int
+    pg: py5.Py5Graphics
+    largura: int
+    altura: int
+    fundo: tuple[int, int, int]
+
+
+GRADES: list[list[Celula]] = []
+PASSO: int = 100
+QUADRADO: int = 100
+DIFF_PASSO_QUADRADO: int = PASSO - QUADRADO
+MAX_INTERNO: int = 10
+PASSO_INTERNO: float = QUADRADO / MAX_INTERNO
+
+
+def setup():
+    py5.size(helpers.LARGURA, helpers.ALTURA, py5.P3D)
+    py5.color_mode(py5.HSB, 360, 100, 100)
+    py5.background(0)
+    params = [
+        (-400 + (i * PASSO / 2), 400 - (i * PASSO / 2), PASSO) for i in range(0, 5)
+    ]
+    for idx, (pi, pf, passo) in enumerate(params):
+        GRADES.append([])
+        pi = int(pi)
+        pf = int(pf)
+        for x in range(pi, pf, passo):
+            for y in range(pi, pf, passo):
+                pg = py5.create_graphics(QUADRADO, QUADRADO)
+                for _ in range(py5.random_int(2, MAX_INTERNO)):
+                    h = 5 + (idx * 30)
+                    s = 100
+                    b = py5.random_int(
+                        80 - int(40 / (idx + 1)), 100 - int(40 / (idx + 1))
+                    )
+                    cor = [h, s, b]
+                celula = Celula(x, y, pg, QUADRADO, QUADRADO, cor)
+                GRADES[idx].append(celula)
+
+
+def draw():
+    py5.background(0)
+    for idx, grade in enumerate(GRADES):
+        angulo = py5.radians(((idx % 4) + (idx + 1)) * 15)
+        with py5.push_matrix():
+            py5.translate(py5.width / 2, py5.height / 2)
+            py5.rotate(angulo)
+            for _, celula in enumerate(grade):
+                pg = celula.pg
+                pg.begin_draw()
+                pg.shape_mode(py5.CENTER)
+                pg.background(*celula.fundo)
+                pg.end_draw()
+                x = celula.x + (DIFF_PASSO_QUADRADO / 2)
+                y = celula.y + (DIFF_PASSO_QUADRADO / 2)
+                with py5.push_style():
+                    pg.fill("#000")
+                    pg.stroke("#000")
+                    py5.square(x - 4, y - 4, QUADRADO + 8)
+                py5.image(pg, x, y)
+    helpers.write_legend(sketch=sketch, frame="#000")
+
+
+def key_pressed():
+    key = py5.key
+    if key == " ":
+        save_and_close()
+
+
+def save_and_close():
+    py5.no_loop()
+    helpers.save_sketch_image(sketch)
+    py5.exit_sketch()
+
+
+if __name__ == "__main__":
+    py5.run_sketch()
