edd6a56b764dbe0cfb0f0e977f9b0b7a456098da
Érico Andrei <ericof@gmail.com>
2024-09-20 14:39:15 +0300

    Sketch for 2024-09-20

--- README.md
+++ README.md
@@ -225,7 +225,9 @@ You can follow this project also on:
 | [2024-09-17](./sketches/d2024_09_17) | Esfera 01 | [./sketches/d2024_09_17/__main__.py](./sketches/d2024_09_17/__main__.py) | ![2024-09-17](./sketches/d2024_09_17/2024-09-17.png) |
 | [2024-09-18](./sketches/d2024_09_18) | Esfera 02 | [./sketches/d2024_09_18/__main__.py](./sketches/d2024_09_18/__main__.py) | ![2024-09-18](./sketches/d2024_09_18/2024-09-18.png) |
 | [2024-09-19](./sketches/d2024_09_19) | Esfera 03 | [./sketches/d2024_09_19/__main__.py](./sketches/d2024_09_19/__main__.py) | ![2024-09-19](./sketches/d2024_09_19/2024-09-19.png) |
+| [2024-09-20](./sketches/d2024_09_20) | Esfera 04 | [./sketches/d2024_09_20/__main__.py](./sketches/d2024_09_20/__main__.py) | ![2024-09-20](./sketches/d2024_09_20/2024-09-20.png) |
 <!-- Next Item -->
+
 
 
 
---
+++ sketches/d2024_09_20/__main__.py
@@ -0,0 +1,70 @@
+"""2024-09-20
+Esfera 04
+Estudo sobre esferas multicoloridas
+png
+Sketch,py5,CreativeCoding
+"""
+
+import numpy as np
+import py5
+
+from utils import helpers
+
+sketch = helpers.info_for_sketch(__file__, __doc__)
+
+raio: int = 300
+pontos = []
+
+
+def calcula_esfera(num: int, raio: int = 200):
+    indices = np.arange(0, num, dtype=float) + 0.5
+    phi = np.arccos(1 - 2 * indices / num)
+    theta = np.pi * (20 + (5**0.5)) * indices
+
+    x, y, z = np.cos(theta) * np.sin(phi), np.sin(theta) * np.sin(phi), np.cos(phi)
+    pontos = []
+    for i in range(0, num):
+        pontos.append((x[i] * raio, y[i] * raio, z[i] * raio))
+    return pontos
+
+
+def setup():
+    global pontos
+    py5.size(helpers.LARGURA, helpers.ALTURA, py5.P3D)
+    py5.background(0)
+    py5.color_mode(py5.HSB, 360, 100, 100)
+    pontos.extend(calcula_esfera(5000, raio))
+
+
+def draw():
+    py5.background(0)
+    py5.stroke_weight(3)
+    frame = py5.frame_count
+    with py5.push_matrix(), py5.push_style():
+        py5.translate(py5.width / 2, py5.height / 2, 0)
+        py5.rotate_y(py5.radians(frame))
+        py5.rotate_z(py5.radians(frame))
+        for x, y, z in pontos:
+            h = py5.remap(x, -raio, raio, 0, 360)
+            s = py5.remap(x, -raio, raio, 50, 80)
+            b = py5.remap(x, -raio, raio, 50, 80)
+            cor = py5.color(h, s, b)
+            py5.stroke(cor)
+            py5.line(x, y, z, x / 1.2, y / 1.2, z / 1.2)
+    helpers.write_legend(sketch=sketch, frame="#fff", cor="#000")
+
+
+def key_pressed():
+    key = py5.key
+    if key == " ":
+        save_and_close()
+
+
+def save_and_close():
+    py5.no_loop()
+    helpers.save_sketch_image(sketch)
+    py5.exit_sketch()
+
+
+if __name__ == "__main__":
+    py5.run_sketch()
