806e6b69be9913b5c787bf61e96bf1c4b802f5fa
Érico Andrei <ericof@gmail.com>
2025-01-12 01:10:29 +0300

    Sketch for 2025-01-12

--- README.md
+++ README.md
@@ -341,7 +341,9 @@ You can follow this project also on:
 | [2025-01-09](./sketches/d2025_01_09) | Public transport seating | [./sketches/d2025_01_09/__main__.py](./sketches/d2025_01_09/__main__.py) | ![2025-01-09](./sketches/d2025_01_09/2025-01-09.png) |
 | [2025-01-10](./sketches/d2025_01_10) | TAU, TAU, TAU, TAU | [./sketches/d2025_01_10/__main__.py](./sketches/d2025_01_10/__main__.py) | ![2025-01-10](./sketches/d2025_01_10/2025-01-10.png) |
 | [2025-01-11](./sketches/d2025_01_11) | Impossible day | [./sketches/d2025_01_11/__main__.py](./sketches/d2025_01_11/__main__.py) | ![2025-01-11](./sketches/d2025_01_11/2025-01-11.png) |
+| [2025-01-12](./sketches/d2025_01_12) | Subdivision | [./sketches/d2025_01_12/__main__.py](./sketches/d2025_01_12/__main__.py) | ![2025-01-12](./sketches/d2025_01_12/2025-01-12.png) |
 <!-- Next Item -->
+
 
 
 
---
+++ sketches/d2025_01_12/__main__.py
@@ -0,0 +1,65 @@
+"""2025-01-12
+Subdivision
+Grade de círculos dentro de círculos
+png
+Sketch,py5,CreativeCoding,genuary,genuary2025,genuary12
+"""
+
+import py5
+
+from utils import helpers
+from utils.draw import cria_grade
+
+sketch = helpers.info_for_sketch(__file__, __doc__)
+
+
+GRADE = None
+LARGURA = 80
+ALTURA = 75
+MARGEM = -800
+
+
+def setup():
+    py5.size(helpers.LARGURA, helpers.ALTURA, py5.P3D)
+    py5.background(0)
+    py5.color_mode(py5.HSB, 360, 100, 100)
+    GRADE = cria_grade(py5.width, py5.height, MARGEM, MARGEM, LARGURA, ALTURA, True)
+    meia_largura = LARGURA / 2
+    meia_altura = ALTURA / 2
+    with py5.push_matrix():
+        with py5.push_style():
+            x0, y0 = GRADE[0]
+            xf, yf = GRADE[-1]
+            py5.no_stroke()
+            py5.fill(180, 80, 80)
+            py5.rect(
+                x0 + meia_largura, y0 + meia_altura, xf + meia_largura, yf + meia_altura
+            )
+        with py5.push_style():
+            for xb, yb in GRADE:
+                h = (yb / 800) * 40 + 90
+                py5.stroke(h, 30, 30)
+                x = xb + meia_largura
+                y = yb + meia_altura
+                for idx, tamanho in enumerate(range(LARGURA + 3, 3, -10)):
+                    h += idx * 5
+                    s = 0 if (idx % 2) == 1 else 100
+                    py5.fill(h, s, 100)
+                    py5.ellipse(x, y, tamanho, tamanho)
+    helpers.write_legend(sketch=sketch, frame="#000")
+
+
+def key_pressed():
+    key = py5.key
+    if key == " ":
+        save_and_close()
+
+
+def save_and_close():
+    py5.no_loop()
+    helpers.save_sketch_image(sketch)
+    py5.exit_sketch()
+
+
+if __name__ == "__main__":
+    py5.run_sketch()
