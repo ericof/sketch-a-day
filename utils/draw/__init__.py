bcf386cbadd957a62603a936b87cf5f5fa839fcf
Érico Andrei <ericof@gmail.com>
2025-02-09 16:53:15 -0300

    Cria gera_octagono

--- utils/draw/__init__.py
+++ utils/draw/__init__.py
@@ -1,5 +1,6 @@
 from .circulo import pontos_circulo  # noQA
 from .cores import rgb_to_hsb  # noQA
 from .formas import gera_hexagono  # noQA
+from .formas import gera_octagono  # noQA
 from .grade import cria_grade  # noQA
 from .grade import cria_grade_ex  # noQA
