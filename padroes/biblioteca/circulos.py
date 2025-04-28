9e15e225f8b90f72e4fbbc37bdf4f522666d7d2d
Érico Andrei <ericof@gmail.com>
2025-04-28 10:46:55 -0300

    Adiciona novos padrões

--- padroes/biblioteca/circulos.py
+++ padroes/biblioteca/circulos.py
@@ -67,3 +67,22 @@ class RaiosCanto(tipos.Padrao):
             pg.no_fill()
             for raio in range(self.largura * 2, 5, -10):
                 pg.circle(*self.centro, raio)
+
+
+@registra_padrao()
+class CirculosRaios(tipos.Padrao):
+    categoria = "circulos"
+
+    def padrao(self, pg: py5.Py5Graphics, cores: tipos.CoresPadrao) -> None:
+        """Implementa padrão."""
+        pg.stroke_weight(self.traco)
+        pg.ellipse_mode(py5.CENTER)
+        passo = 0.75
+        largura_max = self.largura * passo
+        largura_min = 5
+        largura = largura_max
+        with py5.push():
+            pg.no_fill()
+            while largura > largura_min:
+                pg.circle(0, 0, largura)
+                largura *= passo
--- padroes/biblioteca/quadrados.py
+++ padroes/biblioteca/quadrados.py
@@ -73,3 +73,22 @@ class QuadradoVazio(tipos.Padrao):
     def padrao(self, pg: py5.Py5Graphics, cores: tipos.CoresPadrao) -> None:
         """Implementa padrão."""
         pass
+
+
+@registra_padrao()
+class QuadradoRaios(tipos.Padrao):
+    categoria = "quadrados"
+
+    def padrao(self, pg: py5.Py5Graphics, cores: tipos.CoresPadrao) -> None:
+        """Implementa padrão."""
+        pg.stroke_weight(self.traco)
+        pg.rect_mode(py5.CENTER)
+        passo = 0.75
+        largura_max = self.largura * passo
+        largura_min = 5
+        largura = largura_max
+        with py5.push():
+            pg.no_fill()
+            while largura > largura_min:
+                pg.rect(0, 0, largura, largura)
+                largura *= passo
