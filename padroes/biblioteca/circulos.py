f5bd84929a7419991d53bc0075e41f9f374d323f
Érico Andrei <ericof@gmail.com>
2025-05-09 11:17:43 -0300

    Adiciona padrão Circulo3Raios

--- padroes/biblioteca/circulos.py
+++ padroes/biblioteca/circulos.py
@@ -86,3 +86,22 @@ class CirculosRaios(tipos.Padrao):
             while largura > largura_min:
                 pg.circle(0, 0, largura)
                 largura *= passo
+
+
+@registra_padrao()
+class Circulo3Raios(tipos.Padrao):
+    categoria = "circulos"
+
+    def padrao(self, pg: py5.Py5Graphics, cores: tipos.CoresPadrao) -> None:
+        """Implementa padrão."""
+        pg.stroke_weight(self.traco)
+        pg.rect_mode(py5.CENTER)
+        limite = 0.75
+        largura_max = self.largura * limite
+        largura_min = 5
+        meio = (largura_max - largura_min) / 2 + largura_min
+        larguras = [largura_max, meio, largura_min]
+        with py5.push():
+            pg.no_fill()
+            for largura in larguras:
+                pg.circle(0, 0, largura)
