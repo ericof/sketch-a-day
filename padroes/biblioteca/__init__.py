134c379a52b803fcbdff9cc9a25a3097fd2cb3b1
Érico Andrei <ericof@gmail.com>
2025-05-13 10:28:02 -0300

    Adiciona padroes de ondas

--- padroes/biblioteca/__init__.py
+++ padroes/biblioteca/__init__.py
@@ -2,6 +2,8 @@ from ._biblioteca import Biblioteca, registra_padrao  # noQA
 from .circulos import CirculoCentroP  # noQA
 from .circulos import CirculoCanto, CirculoCentroM, CirculoConncentrico
 from .hexagonos import HexagonoRaios  # noQA
+from .ondas import OndasParalelas3  # noQA
+from .ondas import OndasParalelas5  # noQA
 from .quadrados import (  # noQA
     QuadradoCanto,
     QuadradoCantos,
@@ -27,6 +29,8 @@ __all__ = [
     "CirculoCentroP",
     "CirculoConncentrico",
     "HexagonoRaios",
+    "OndasParalelas3",
+    "OndasParalelas5",
     "QuadradoCanto",
     "QuadradoCantos",
     "QuadradoCentro",
---
+++ padroes/biblioteca/ondas.py
@@ -0,0 +1,55 @@
+import py5
+
+from .. import tipos
+from ._biblioteca import registra_padrao
+
+
+class PadraoOndas(tipos.Padrao):
+    categoria = "ondas"
+
+    @property
+    def pontos(self) -> tuple[tuple[float, float], ...]:
+        return tuple()
+
+    def padrao(self, pg: py5.Py5Graphics, cores: tipos.CoresPadrao) -> None:
+        """Implementa padrão."""
+        pg.no_fill()
+        pg.stroke_weight(self.traco)
+        for linha in self.pontos:
+            x0, y0 = None, None
+            for x, y in linha:
+                if x0 is None:
+                    x0, y0 = x, y
+                pg.line(x0, y0, x, y)
+                x0, y0 = x, y
+
+
+class OndasParalelas(PadraoOndas):
+    ciclos: int = 4
+    min_max: tuple[int, int] = (0, 1)
+
+    @property
+    def pontos(self) -> tuple[tuple[float, float], ...]:
+        x_min = int(-self.largura * 3)
+        x_max = int(x_min * -1)
+        passo = (x_max - x_min) / (360 / self.ciclos)
+        distancia = (self.largura / 3) - (self.largura / 2)
+        pontos = []
+        for idy in range(*self.min_max):
+            linha = []
+            yb = distancia * idy
+            for idx, x in enumerate(range(x_min, x_max)):
+                y = int((py5.sin(py5.radians(idx * passo)) * distancia) + yb)
+                linha.append((x, y))
+            pontos.append(tuple(linha))
+        return tuple(pontos)
+
+
+@registra_padrao()
+class OndasParalelas3(OndasParalelas):
+    min_max: tuple[int, int] = (-1, 2)
+
+
+@registra_padrao()
+class OndasParalelas5(OndasParalelas):
+    min_max: tuple[int, int] = (-2, 3)
