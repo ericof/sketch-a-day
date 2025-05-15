fbc449f2368088d573fa8ae112ee8e74257c56c3
Érico Andrei <ericof@gmail.com>
2025-05-15 09:00:27 -0300

    Adiciona padrão de hexágono

--- padroes/biblioteca/hexagonos.py
+++ padroes/biblioteca/hexagonos.py
@@ -29,3 +29,27 @@ class HexagonoRaios(tipos.Padrao):
             y = -largura // 2
             pg.shape(hexagono, 0, y, largura, largura)
             largura *= passo
+
+
+class HexagonoConcentricos(tipos.Padrao):
+    categoria = "hexagonos"
+    min_max: tuple[int, int] = (0, 1)
+
+    def padrao(self, pg: py5.Py5Graphics, cores: tipos.CoresPadrao) -> None:
+        """Implementa padrão."""
+        hexagono = gera_hexagono()
+        hexagono.set_stroke_weight(self.traco)
+        hexagono.set_stroke(cores.traco)
+        hexagono.set_fill(False)
+        items = self.min_max[1] - self.min_max[0]
+        largura_b = self.largura * 0.7
+        passo = largura_b / items
+        for idy in range(*self.min_max):
+            largura = largura_b + (passo * idy)
+            y = -largura // 2
+            pg.shape(hexagono, 0, y, largura, largura)
+
+
+@registra_padrao()
+class HexagonoConcentricos3(HexagonoConcentricos):
+    min_max: tuple[int, int] = (-1, 2)
