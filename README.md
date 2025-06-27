52e472421917188cb1fd1706678bc699a5f2ca95
Érico Andrei <ericof@gmail.com>
2025-06-27 15:55:10 +0300

    Sketch for 2025-06-27

--- README.md
+++ README.md
@@ -507,7 +507,9 @@ You can follow this project also on:
 | [2025-06-24](./sketches/d2025_06_24) | Mosaico 05 | [./sketches/d2025_06_24/__main__.py](./sketches/d2025_06_24/__main__.py) | ![2025-06-24](./sketches/d2025_06_24/2025-06-24.png) |
 | [2025-06-25](./sketches/d2025_06_25) | Mosaico 06 | [./sketches/d2025_06_25/__main__.py](./sketches/d2025_06_25/__main__.py) | ![2025-06-25](./sketches/d2025_06_25/2025-06-25.png) |
 | [2025-06-26](./sketches/d2025_06_26) | Mosaico 07 | [./sketches/d2025_06_26/__main__.py](./sketches/d2025_06_26/__main__.py) | ![2025-06-26](./sketches/d2025_06_26/2025-06-26.png) |
+| [2025-06-27](./sketches/d2025_06_27) | Mosaico 08 | [./sketches/d2025_06_27/__main__.py](./sketches/d2025_06_27/__main__.py) | ![2025-06-27](./sketches/d2025_06_27/2025-06-27.png) |
 <!-- Next Item -->
+
 
 
 
---
+++ sketches/d2025_06_27/__main__.py
@@ -0,0 +1,60 @@
+"""2025-06-27
+Mosaico 08
+Mosaico com retângulos vazados em uma grade desigual.
+png
+Sketch,py5,CreativeCoding
+"""
+
+import py5
+
+from utils import helpers
+from utils.draw import gera_paleta, grade_desigual
+
+sketch = helpers.info_for_sketch(__file__, __doc__)
+
+
+def setup():
+    py5.size(helpers.LARGURA, helpers.ALTURA, py5.P3D)
+    fundo = py5.color(255)
+    py5.background(fundo)
+    py5.stroke_weight(5)
+    py5.stroke(0)
+    py5.ellipse_mode(py5.RADIUS)
+    paleta = gera_paleta("mondrian", True)
+    grade = grade_desigual(py5.width, py5.height, 1000, 6000, 2, 6)
+    meio_x = py5.width // 2
+    meio_y = py5.height // 2
+    with py5.push():
+        py5.translate(meio_x, meio_y, -80)
+        for xb, yb, largura, altura in grade:
+            x = xb - meio_x
+            y = yb - meio_y
+            cor = paleta[0]
+            with py5.push():
+                py5.fill(cor)
+                py5.rect(x, y, largura, altura)
+            paleta.rotate()
+            with py5.push():
+                x += largura // 2
+                y += altura // 2
+                cor = paleta[0]
+                py5.stroke_weight(1)
+                py5.fill(fundo)
+                py5.ellipse(x, y, largura / 4, altura / 4)
+    helpers.write_legend(sketch=sketch, frame="#000", cor="#fff")
+
+
+def key_pressed():
+    key = py5.key
+    if key == " ":
+        save_and_close()
+
+
+def save_and_close():
+    py5.no_loop()
+    helpers.save_sketch_image(sketch)
+    py5.exit_sketch()
+
+
+if __name__ == "__main__":
+    py5.run_sketch()
