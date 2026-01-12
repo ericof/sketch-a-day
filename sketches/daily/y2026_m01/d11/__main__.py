c1f3d8b7ebea1eaa5011e8054d7ae93f914fe95f
Érico Andrei <ericof@gmail.com>
2026-01-11 22:27:28 -0300

    Sketch for 2026-01-11

---
+++ sketches/daily/y2026_m01/d11/__main__.py
@@ -0,0 +1,42 @@
+"""2026-01-11
+Description
+Alt
+ericof.com
+png
+Sketch,py5,CreativeCoding
+"""
+
+from sketches.utils.draw import canvas
+from sketches.utils.helpers import sketches as helpers
+
+import py5
+
+
+sketch = helpers.info_for_sketch(__file__, __doc__)
+
+
+def setup():
+    py5.size(*helpers.DIMENSOES.external, py5.P3D)
+    cor_fundo = py5.color(0)
+    py5.background(cor_fundo)
+    py5.color_mode(py5.HSB, 360, 100, 100)
+    # Credits and go
+    canvas.sketch_frame(
+        sketch, cor_fundo, "large_transparent_white", "transparent_white"
+    )
+
+
+def key_pressed():
+    key = py5.key
+    if key == " ":
+        save_and_close()
+
+
+def save_and_close():
+    py5.no_loop()
+    canvas.save_sketch_image(sketch)
+    py5.exit_sketch()
+
+
+if __name__ == "__main__":
+    py5.run_sketch()
