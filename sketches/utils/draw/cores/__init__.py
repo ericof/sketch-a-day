b4bafea4b86aa30363b48db277f302a179324865
Érico Andrei <ericof@gmail.com>
2026-02-26 10:21:12 -0300

    Adiciona funções extrai_rgb, extrai_rgba, lerp_color_rgba

--- sketches/utils/draw/cores/__init__.py
+++ sketches/utils/draw/cores/__init__.py
@@ -1,4 +1,7 @@
+from functools import cache
+
 import numpy as np
+import py5
 
 
 def rgb_to_hsb(r: float, g: float, b: float) -> list[float]:
@@ -36,3 +39,26 @@ def rgb_to_hsb(r: float, g: float, b: float) -> list[float]:
         float(brightness[0]) * 100,
     ]
     return hsb
+
+
+def extrai_rgb(cor: int) -> tuple[float, float, float]:
+    """Extrai os componentes R G B de uma cor."""
+    return (py5.red(cor), py5.green(cor), py5.blue(cor))
+
+
+def extrai_rgba(cor: int) -> tuple[float, float, float, float]:
+    """Extrai os componentes R G B de uma cor."""
+    return (py5.red(cor), py5.green(cor), py5.blue(cor), py5.alpha(cor))
+
+
+@cache
+def lerp_color_rgba(cor_1: int, cor_2: int, t) -> int:
+    """Calcula uma cor intermediária entre duas cores."""
+    r1, g1, b1, a1 = extrai_rgba(cor_1)
+    r2, g2, b2, a2 = extrai_rgba(cor_2)
+    t = py5.constrain(t, 0, 1)
+    r = float(py5.lerp(r1, r2, t))
+    g = float(py5.lerp(g1, g2, t))
+    b = float(py5.lerp(b1, b2, t))
+    a = float(py5.lerp(a1, a2, t))
+    return py5.color(r, g, b, a)
