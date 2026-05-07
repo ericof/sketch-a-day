a308530c830cbbcf493efafaaec83d4c51b1c2a6
Érico Andrei <ericof@gmail.com>
2026-05-07 15:18:46 -0300

    Corrige bug na geração de polígonos regulares

--- sketches/utils/draw/formas.py
+++ sketches/utils/draw/formas.py
@@ -8,8 +8,9 @@ def _gera_forma(lados: int = 3) -> py5.Py5Shape:
     pontos = []
     x, y = 0, 0
     largura = 1
-    passo = 360 // lados
-    for angulo in range(0, 360, passo):
+    passo = 360 / lados
+    for i in range(lados):
+        angulo = i * passo
         x += np.cos(py5.radians(angulo)) * largura
         y += np.sin(py5.radians(angulo)) * largura
         pontos.append((x, y))
